67a9f0a545ae245adf3bd8c0600afbbbf9625931
thnbi <renato_freitas13@icloud.com>
2022-12-14 17:06:40 -0300

    refactor: migrating TextContainer component from js to ts

---
+++ src/components/TextContainer/index.tsx
@@ -0,0 +1,29 @@
+import "./style.css";
+
+interface TextContainerProps {
+	onInput: (value: string) => void;
+	placeholder: string;
+	label: string;
+	value: string;
+	required: boolean;
+}
+
+const TextContainer = ({label, onInput, placeholder, required, value}: TextContainerProps) => {
+	const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		onInput(event.target.value);
+	};
+
+	return (
+		<div className="campo-texto">
+			<label>{label}</label>
+			<input
+				value={value}
+				required={required}
+				placeholder={`${placeholder}...`}
+				onChange={inputHandler}
+			/>
+		</div>
+	);
+};
+
+export default TextContainer;
